5010f4a7dc754c27dba3e68531f901d8ed5544b2
Maria Moran <mariamoranluaces@gmail.com>
2025-08-08 18:11:05 +0200

    feat: add sidebar visibility logic based on current route

--- src/layouts/app-layout/app-layout.layout.tsx
+++ src/layouts/app-layout/app-layout.layout.tsx
@@ -1,13 +1,27 @@
 import { bind } from "@/core/styles/bind";
 import styles from "./app-layout.module.scss";
 import { NavigationBar } from "@/core/components/navigation-bar/navigation-bar.component";
+import { ROUTES } from "@/core/routes/routes";
+import { useLocation } from "react-router-dom";
 const cn = bind(styles);
 
+const routeHasSidebar = (route: string) => {
+  return [
+    ROUTES.HOME,
+    ROUTES.FAVORITES,
+    ROUTES.SETTINGS,
+    ROUTES.STATISTICS,
+  ].includes(route);
+};
+
 export const AppLayout = ({ children }: React.PropsWithChildren) => {
+  const locale = useLocation();
+  const hasSidebar = routeHasSidebar(locale.pathname);
+
   return (
     <div className={cn("app-layout")}>
       <main className={cn("app-layout__main")}>{children}</main>
-      <NavigationBar />
+      {hasSidebar ? <NavigationBar /> : null}
     </div>
   );
 };
